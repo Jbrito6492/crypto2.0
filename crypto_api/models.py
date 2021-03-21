0e87e06fc8963a1b76da4e3da4227be203e66237
Joshua Brito <joshuabrito@Joshuas-MacBook-Pro.local>
2021-03-20 22:15:57 -0700

    created migration for crypto_api

--- crypto_api/models.py
+++ crypto_api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Crypto(models.Model):
+    """General crypto class"""
+    ticker = models.CharField(max_length=25, unique=True)
+    name = models.CharField(max_length=25)
+    description = models.CharField(max_length=250)
